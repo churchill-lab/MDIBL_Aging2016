c60f253bf7baaa768a9c95882424e1da8f832499
Daniel Gatti <Dan.Gatti@jax.org>
2016-06-09 10:58:59 -0400

    Create download_data_from_ftp.sh

---
+++ scripts/download_data_from_ftp.sh
@@ -0,0 +1,18 @@
+# make /data folder and subfolders
+mkdir -p /data
+mkdir -p /tutorial
+
+# Copy the data directory.
+wget --directory-prefix=/data ftp://ftp.jax.org/dgatti/IBANGS2016/data/DO_Sanger_SDPs.txt.bgz
+wget --directory-prefix=/data ftp://ftp.jax.org/dgatti/IBANGS2016/data/DO_Sanger_SDPs.txt.bgz.tbi
+wget --directory-prefix=/data ftp://ftp.jax.org/dgatti/IBANGS2016/data/assoc_perms.rds
+wget --directory-prefix=/data ftp://ftp.jax.org/dgatti/IBANGS2016/data/linkage_perms.rds
+
+# Copy the tutorial directory.
+wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/IBANGS2016/markdown/DO.impute.founders.sm.png
+wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/IBANGS2016/markdown/DOQTL_workshop_IBANGS2016.Rmd
+wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/IBANGS2016/markdown/DOQTL_workshop_IBANGS2016.html
+wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/IBANGS2016/markdown/DOQTL_workshop_IBANGS2016.R
+
+# set privilages - everybody can do everything
+chmod --recursive 777 /ibangs
