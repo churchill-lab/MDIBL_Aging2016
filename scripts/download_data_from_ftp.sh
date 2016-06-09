5dd973a2bb2f12ba0e1baffc45fab129f51fb4a1
Daniel Gatti <Dan.Gatti@jax.org>
2016-06-09 14:30:33 -0400

    Added figures.

--- scripts/download_data_from_ftp.sh
+++ scripts/download_data_from_ftp.sh
@@ -16,7 +16,13 @@ wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DOQTL
 wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DOQTL_demo.R
 
 # Copy figures.
-wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DOQTL_demo.R
+wget --directory-prefix=/tutorial/figure ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/figure/enzene_study_design.png
+wget --directory-prefix=/tutorial/figure ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/figure/DO.impute.founders.sm.png
+wget --directory-prefix=/tutorial/figure ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/figure/EnsEMBL.Sult3a1.png
+wget --directory-prefix=/tutorial/figure ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/figure/EnsEMBL_Sult3a1_Gm4794_paralog.png
+wget --directory-prefix=/tutorial/figure ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/figure/French.et.al.Figure3.png
+wget --directory-prefix=/tutorial/figure ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/figure/French.et.al.Sup.Figure2.png
+wget --directory-prefix=/tutorial/figure ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/figure/French.et.al.Sup.Figure3.png
 
 # set privilages - everybody can do everything
 chmod --recursive 777 /data
