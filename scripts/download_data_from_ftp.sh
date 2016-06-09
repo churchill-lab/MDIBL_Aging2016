d14fcd581562a4468564a57c68fab9416c3ee298
Daniel Gatti <Dan.Gatti@jax.org>
2016-06-09 11:03:20 -0400

    Update download_data_from_ftp.sh

--- scripts/download_data_from_ftp.sh
+++ scripts/download_data_from_ftp.sh
@@ -7,12 +7,14 @@ wget --directory-prefix=/data ftp://ftp.jax.org/dgatti/IBANGS2016/data/DO_Sanger
 wget --directory-prefix=/data ftp://ftp.jax.org/dgatti/IBANGS2016/data/DO_Sanger_SDPs.txt.bgz.tbi
 wget --directory-prefix=/data ftp://ftp.jax.org/dgatti/IBANGS2016/data/assoc_perms.rds
 wget --directory-prefix=/data ftp://ftp.jax.org/dgatti/IBANGS2016/data/linkage_perms.rds
+wget --directory-prefix=/data ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DOQTL_demo.Rdata
 
 # Copy the tutorial directory.
 wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/markdown/DO.impute.founders.sm.png
 wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/markdown/DOQTL_workshop_MDIBL_Aging2016.Rmd
 wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/markdown/DOQTL_workshop_MDIBL_Aging2016.html
 wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/markdown/DOQTL_workshop_MDIBL_Aging2016.R
+
 
 # set privilages - everybody can do everything
 chmod --recursive 777 /ibangs
