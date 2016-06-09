26d5b3d6cdec9bf0b8f51e5799831659695878e1
Daniel Gatti <Dan.Gatti@jax.org>
2016-06-09 14:17:04 -0400

    Removed markdown directory from ftp line

--- scripts/download_data_from_ftp.sh
+++ scripts/download_data_from_ftp.sh
@@ -9,10 +9,10 @@ wget --directory-prefix=/data ftp://ftp.jax.org/dgatti/IBANGS2016/data/assoc_per
 wget --directory-prefix=/data ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DOQTL_demo.Rdata
 
 # Copy the tutorial directory.
-wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/markdown/DO.impute.founders.sm.png
-wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/markdown/DOQTL_demo.Rmd
-wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/markdown/DOQTL_demo.html
-wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/markdown/DOQTL_demo.R
+wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DO.impute.founders.sm.png
+wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DOQTL_demo.Rmd
+wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DOQTL_demo.html
+wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DOQTL_demo.R
 
 # set privilages - everybody can do everything
 chmod --recursive 777 /data
