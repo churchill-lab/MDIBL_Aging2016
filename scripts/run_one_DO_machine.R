50cea0d2234aa1342af85c0abd2211c1db0f4ecc
Daniel Gatti <Dan.Gatti@jax.org>
2016-06-09 10:57:11 -0400

    Update run_one_DO_machine.R

--- scripts/run_one_DO_machine.R
+++ scripts/run_one_DO_machine.R
@@ -10,19 +10,19 @@ d %>% docklet_pull("churchill/ibangs2016")
 d %>% docklet_images()
 
 # download files to /data folder, takes ~30mins
-lines <- "wget https://raw.githubusercontent.com/churchill-lab/IBANGS2016/master/scripts/download_data_from_ftp.sh
+lines <- "wget https://raw.githubusercontent.com/churchill-lab/MDIBL_Aging2016/master/scripts/download_data_from_ftp.sh
           /bin/bash download_data_from_ftp.sh
           rm download_data_from_ftp.sh"
 cmd <- paste0("ssh ", analogsea:::ssh_options(), " ", "root", "@", analogsea:::droplet_ip(d)," ", shQuote(lines))
 analogsea:::do_system(d, cmd, verbose = TRUE)
 
 # start the containers
-d %>% docklet_run("-d", " -v /ibangs/data:/ibangs/data", " -v /ibangs/tutorial:/ibangs/tutorial", " -p 8787:8787", 
-                  " -e USER=rstudio", " -e PASSWORD=ibangs ", "--name myrstudio ", "churchill/ibangs2016")
+d %>% docklet_run("-d", " -v /data:/data", " -v /tutorial:/tutorial", " -p 8787:8787", 
+                  " -e USER=rstudio", " -e PASSWORD=mdibl ", "--name myrstudio ", "churchill/mdibl2016")
 
 # add symbolic links
-lines2 <- "docker exec myrstudio ln -s /ibangs/data /home/rstudio/data
-           docker exec myrstudio ln -s /ibangs/tutorial /home/rstudio/tutorial"
+lines2 <- "docker exec myrstudio ln -s /data /home/rstudio/data
+           docker exec myrstudio ln -s tutorial /home/rstudio/tutorial"
 cmd2 <- paste0("ssh ", analogsea:::ssh_options(), " ", "root", "@", analogsea:::droplet_ip(d)," ", shQuote(lines2))
 analogsea:::do_system(d, cmd2, verbose = TRUE)
 
