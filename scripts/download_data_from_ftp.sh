8a6aeaba06a59bcd5505f9ce573fa4079d62600f
Daniel Gatti <Dan.Gatti@jax.org>
2016-06-09 14:28:23 -0400

    Update download_data_from_ftp.sh

--- scripts/download_data_from_ftp.sh
+++ scripts/download_data_from_ftp.sh
@@ -1,6 +1,7 @@
 # make /data folder and subfolders
 mkdir -p /data
 mkdir -p /tutorial
+mkdir -p /tutorial/figure
 
 # Copy the data directory.
 wget --directory-prefix=/data ftp://ftp.jax.org/dgatti/IBANGS2016/data/DO_Sanger_SDPs.txt.bgz
@@ -12,6 +13,9 @@ wget --directory-prefix=/data ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DOQTL_dem
 wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DO.impute.founders.sm.png
 wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DOQTL_demo.Rmd
 wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DOQTL_demo.html
+wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DOQTL_demo.R
+
+# Copy figures.
 wget --directory-prefix=/tutorial ftp://ftp.jax.org/dgatti/MDIBL_Aging2016/DOQTL_demo.R
 
 # set privilages - everybody can do everything
