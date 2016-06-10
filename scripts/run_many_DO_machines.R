7fe39d1d516d3fec96c46ee9c9b1f4a8ded8281f
Daniel Gatti <Dan.Gatti@jax.org>
2016-06-10 10:00:15 -0400

    Update run_many_DO_machines.R

--- scripts/run_many_DO_machines.R
+++ scripts/run_many_DO_machines.R
@@ -7,60 +7,14 @@ participants <- read.csv("participant_list_mdibl_aging2016.csv", as.is=TRUE)
 N = nrow(participants)
 
 # Single machine.
-img = images(private = TRUE)[["churchill/mdibl2016"]]
-d = droplet_create(name = "droplet1", size = "8gb", image = img[["id"]],
-                   region = "nyc2")
+#img = images(private = TRUE)[["churchill/mdibl2016"]]
+#d = droplet_create(name = "droplet1", size = "8gb", image = img[["id"]],
+#                   region = "nyc2")
 
 # Trying new command to make multiple machines at once.
 img = images(private = TRUE)[["churchill/mdibl2016"]]
-droplet.list = droplets_create(names = paste0("Machine", 1:20), size = "8gb", image = img[["id"]],
+droplet_list = droplets_create(names = paste0("Machine", 1:20), size = "8gb", image = img[["id"]],
                                region = "nyc2")
-
-# create a droplet for each participant
-droplet_list <- list()
-
-# parallelization would cause API error
-for(i in 1:N) {
-  print(i)
-  # start i-th machine
-#  droplet_list[[i]] <- docklet_create(size = getOption("do_size", "8gb"), 
-#                                      region = getOption("do_region", "nyc2"))
-  droplet_list[[i]] <- droplet_create(size = getOption("do_size", "8gb"), 
-                                      region = getOption("do_region", "nyc2"),
-                                      image = getOption("do_image", "churchill/mdibl2016"))
-}
-
-cl <- makeCluster(N)
-registerDoParallel(cl)
-
-
-# pulling docker images
-foreach(i = 1:N, .packages="analogsea") %dopar% {
-  
-  # select droplet
-  d = droplet_list[[i]]
-  
-  # pull docker images
-#  d %>% docklet_pull("rocker/hadleyverse")
-#  d %>% docklet_pull("churchill/ibangs2016")
-  d %>% docklet_pull("churchill/mdibl2016")
-  d %>% docklet_images()
-}
-
-# download files to /data folder, takes ~1 hour
-#foreach(i = 1:N, .packages="analogsea") %dopar% {
-  
-  # select droplet
-#  d = droplet_list[[i]]
-  
-#  lines <- "wget https://raw.githubusercontent.com/churchill-lab/IBANGS2016/master/scripts/download_data_from_ftp.sh
-#            /bin/bash download_data_from_ftp.sh
-#            rm download_data_from_ftp.sh"
-#  cmd <- paste0("ssh ", analogsea:::ssh_options(), " ", "root", "@", analogsea:::droplet_ip(d)," ", shQuote(lines))
-#  analogsea:::do_system(d, cmd, verbose = TRUE)
-#}
-
-#stopCluster(cl)
 
 # start docker containers
 for(i in 1:N) {
@@ -68,15 +22,16 @@ for(i in 1:N) {
   # select droplet
   d = droplet_list[[i]]
   
+  # start the container.
   d %>% docklet_run("-d", " -v /data:/data", " -v /tutorial:/tutorial", " -p 8787:8787", 
-                    " -e USER=rstudio", " -e PASSWORD=mdibl ", "--name myrstudio ", "churchill/mdibl2016")
+                    " -e USER=rstudio", " -e PASSWORD=mdibl ", "--name myrstudio ", "churchill/ibangs2016")
 
   # add symbolic links
   lines2 <- "docker exec myrstudio ln -s /data /home/rstudio/data
              docker exec myrstudio ln -s /tutorial /home/rstudio/tutorial"
   cmd2 <- paste0("ssh ", analogsea:::ssh_options(), " ", "root", "@", analogsea:::droplet_ip(d)," ", shQuote(lines2))
-  analogsea:::do_system(d, cmd2, verbose = TRUE)  
-}
+  analogsea:::do_system(d, cmd2, verbose = TRUE)
+} # for(i)
 
 ### Create participant table with links
 participants$DO_machine <- sapply(droplet_list, function(x) x$name)
